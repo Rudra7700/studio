ffed72a948c16dbc63fd1ecc8f1a6f01efee4f07
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-20 11:55:06 +0000

    SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5001/agrisprayer-ai/us-central1/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
