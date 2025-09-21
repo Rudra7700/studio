ffecc02ad7c87d864de679091bc91d90637c2c71
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-21 13:30:56 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {genkitEval} from '@genkit-ai/eval';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       tools: ['searchWeb'],
     }),
-    genkitEval(),
   ],
   model: 'googleai/gemini-2.5-flash',
 });
