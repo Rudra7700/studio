ab3c77c8e0970d7739996bd843bc8a9fb490e75f
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-21 16:54:53 +0000

    it is  just empty section ? be creative man

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -6,3 +6,4 @@ import '@/ai/flows/summarize-field-health.ts';
 import '@/ai/flows/get-treatment-recommendations.ts';
 import '@/ai/flows/show-mandi-price.ts';
 import '@/ai/flows/generate-crop-image.ts';
+
