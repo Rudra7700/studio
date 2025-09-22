adeac222989504bab60e93d955be45e602ae83b0
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-22 14:26:10 +0000

    it taking so many time and just showing saving... (_for element div_)

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -294,3 +295,5 @@ export default function DashboardSettingsPage() {
     </Form>
   );
 }
+
+    
