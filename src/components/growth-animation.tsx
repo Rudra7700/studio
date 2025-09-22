d6b58cd165f81395ff5b1f309b51dcc3733a00ab
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-22 13:17:01 +0000

    Try fixing this error: `Console Error: React does not recognize the `str

--- src/components/growth-animation.tsx
+++ src/components/growth-animation.tsx
@@ -11,8 +11,8 @@ const SeedIcon = () => (
 const SproutIcon = () => (
   <svg width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-green-600">
     <path d="M7 20h10" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M12 20V4a2 2 0 0 1 2-2h0a2 2 0 0 1 2 2v2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin-round="round"/>
-    <path d="M12 4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin-round="round"/>
+    <path d="M12 20V4a2 2 0 0 1 2-2h0a2 2 0 0 1 2 2v2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M12 4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
 
