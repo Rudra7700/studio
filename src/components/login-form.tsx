331c3efc418cfc082edb2fb06d539d84682e4512
Rudrasis Panda <rudrasispanda777@gmail.com>
2025-09-23 15:45:27 +0000

    fix this (_for element <Button>_)

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -27,7 +27,7 @@ const loginSchema = z.object({
 });
 
 const GoogleIcon = () => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4">
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2">
         <title>Google</title>
         <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.05 1.05-2.36 1.67-4.18 1.67-4.97 0-9-4.03-9-9s4.03-9 9-9c2.39 0 4.5 1.02 6.13 2.56l2.34-2.34C18.63 1.19 15.82 0 12.48 0 5.88 0 0 5.88 0 12.48s5.88 12.48 12.48 12.48c6.92 0 11.7-4.72 11.7-12.06 0-.76-.06-1.52-.18-2.28H12.48z"/>
     </svg>
@@ -91,7 +91,7 @@ export function LoginForm() {
     <div className="grid gap-6">
       <Button variant="outline" onClick={handleGoogleSignIn} disabled={isLoading || isGoogleLoading}>
         {isGoogleLoading ? <Loader2 className="mr-2 animate-spin" /> : <GoogleIcon />}
-        Google
+        Sign in with Google
       </Button>
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
